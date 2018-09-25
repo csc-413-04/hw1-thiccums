fdab3bbef59bae520025c275c05256a39ec10d12
Tyler Floyd <inglorious_rex@live.com>
2018-09-25 14:41:11 -0700

    Database Class Created
    Created the database class to begin work in.

---
+++ src/simpleserver/Database.java
@@ -0,0 +1,15 @@
+package simpleserver;
+
+import java.util.*;
+
+public class Database {
+
+    Map<String, String> db = new HashMap<>();
+
+
+    public void populate(String data){
+
+    };
+}
+
+
