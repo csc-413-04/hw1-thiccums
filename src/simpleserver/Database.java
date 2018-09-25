6a357a90b963708d948ab18b5d03b164afe32f2c
Tyler Floyd <inglorious_rex@live.com>
2018-09-25 15:46:26 -0700

    Added a search function to database

--- src/simpleserver/Database.java
+++ src/simpleserver/Database.java
@@ -7,14 +7,28 @@ public class Database {
     Set<Object> db = new HashSet<>();
 
 
-    public void populate(Object object){      //temporary method just using as a placeholder
+    public void populate(Object[] objArr){      //temporary method just using as a placeholder
 
-        db.add(object);
-    };
-
-    public void findValue(Object object){                  //placeholder method for finding and printing values for a "/user" or key along those lines
+        for(int i = 0; i < objArr.length; i++)
+        db.add(objArr[i]);
 
     }
+
+    public Object findValue(String value) {                  //placeholder method for finding and printing values for a "/user" or key along those lines
+
+        Iterator<Object> it = db.iterator();
+
+        while(it.hasNext()){
+
+            if(it.toString()== value){
+               break;
+            }
+
+        }
+        
+            return it;
+    }
+
 }
 
 
