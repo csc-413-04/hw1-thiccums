b09096aaf6485c5df90b649b1f662eb85d37d42e
Tyler Floyd <inglorious_rex@live.com>
2018-09-26 19:36:16 -0700

    Database Work

--- src/simpleserver/Database.java
+++ src/simpleserver/Database.java
@@ -1,32 +1,33 @@
 package simpleserver;
 
 import java.util.*;
+import java.lang.*;
 
 public class Database {
 
-    Set<Object> db = new HashSet<>();
+    Map<Class, Object> db =  new HashMap<Class, Object>();
 
 
     public void populate(Object[] objArr){      //temporary method just using as a placeholder
 
-        for(int i = 0; i < objArr.length; i++)
-        db.add(objArr[i]);
+        for(int i = 0; i < objArr.length; i++){
+            db.put(objArr[i].getClass(), objArr[i]);
+        }
+
 
     }
 
-    public Object findValue(String value) {                  //placeholder method for finding and printing values for a "/user" or key along those lines
+    public Object findValue(Class keyValue, Object value) {                  //placeholder method for finding and printing values for a "/user" or key along those lines
 
-        Iterator<Object> it = db.iterator();
+        for(Object key : db.values()){
 
-        while(it.hasNext()){
-
-            if(it.toString()== value){
-               break;
+            if(key == value){
+               return key;
             }
 
         }
-        
-            return it;
+
+
     }
 
 }
