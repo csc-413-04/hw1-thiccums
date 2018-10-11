6d28e135df14597ef8d894b58188edb493495205
loucid <syd.morano@gmail.com>
2018-10-10 20:37:33 -0700

    more updating stuff
    Just more stuff with updating from the lecture with an added switch statment

--- src/simpleserver/ProcessorFactory.java
+++ src/simpleserver/ProcessorFactory.java
@@ -1,25 +1,20 @@
 package simpleserver;
 
-class ProcessorFactory {
-
-    public static String makeProcessor(Request rq, Database data) {
-        if(!(rq.getValid())) {
-            return (new Response("Error")).convertToJson();
-        }
-        switch (rq.getEndpoint()) {
-                case "user":
-                    return UserProcess.response(rq, data).convertToJson();
-                case "posts":
-                    return PostProcess.response(rq, data).convertToJson();
-        }
-        return  null;
-    }
-}
-
 public class ProcessFactory {
-    static ServerProcessor getProcessor(String rawEndpoint){
-        String endpoint = "/test";
-        String querey = "/somthin";
-        //swtich statements
+
+    static ServerProcessor getProcessor(String url) {
+      String endpoint = "/test";
+      String query = "?asd=hello";
+      ServerProcessor processor = null;
+
+      switch (rq.getEndpoint()) {
+        case "user":
+            return UserProcess.response(rq, data).convertToJson();
+        case "post":
+            return PostProcess.response(rq, data).convertToJson();
+}
+      processor = new UserProcessor();
+
+      return processor;
     }
 }
