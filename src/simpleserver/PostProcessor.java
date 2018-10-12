96156399e53909f7ac62bf4a298410d12b526963
acheron7 <dadams03@gmail.com>
2018-10-11 18:06:30 -0700

    Updates PostProcessor to use db to get data for response

--- src/simpleserver/PostProcessor.java
+++ src/simpleserver/PostProcessor.java
@@ -5,13 +5,17 @@ import com.google.gson.Gson;
 public class PostProcessor implements Processor {
 
     @Override
-    public String process(String query){
+    public String process(String query) {
+        // All logic goes in here
         Response response = new Response();
         Database db = Database.getDatabase();
-        response.setData(db.getAllUsers());
-        response.setStatus("Ok");
+        int postid = Integer.parseInt(query);
+        response.setData(db.getPost(postid)); // response's data gets post data from db
+        response.setStatus("OK");  // No error checking :(
         Gson gson = new Gson();
         return gson.toJson(response);
     }
+
+}
 
 }
